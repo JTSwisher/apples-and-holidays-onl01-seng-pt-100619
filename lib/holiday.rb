4d75f560dc58246f77e68b3e426c949c12e7cd10
Jeff Swisher <jefftswisher@gmail.com>
2019-10-16 23:48:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,10 +22,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
   holiday_hash.each do |season, holiday|
     if season == :summer 
       holiday.each do |data|
-       print data[1]
       end   
-    end
-  end   
+  
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
