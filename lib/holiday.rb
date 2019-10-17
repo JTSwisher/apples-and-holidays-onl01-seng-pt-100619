8494a261dea33d61afbd6519acdadf81851a1d54
Jeff Swisher <jefftswisher@gmail.com>
2019-10-17 00:41:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,7 +35,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   # add the second argument to the memorial day array
   holiday_hash[:memorial_day] do |holiday, party_favor|
   party_favor << supply 
-end
+ end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
