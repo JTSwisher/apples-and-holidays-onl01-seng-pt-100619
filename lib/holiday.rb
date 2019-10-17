b0d2df4b6c2dd8b54b6708330be7895102320207
Jeff Swisher <jefftswisher@gmail.com>
2019-10-17 01:59:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,6 +62,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
       holiday_hash.each do |season, holidays|
       puts "#{season.capitalize}:"
+      
+  end 
 end
 
 def all_holidays_with_bbq(holiday_hash)
